1cb63d092cf50a6343a0c3448a3b54601f661f43
Austen Bright <pureaustenbright@gmail.com>
2020-03-14 23:03:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,6 @@
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+phrase = "'It's-a me, Mario!'"
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
