6fc2970fd6ef0e40e37a1285834f998fc3846f50
Austen Bright <pureaustenbright@gmail.com>
2020-03-14 23:05:32 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,7 @@
-status = "'Thank You Mario! But Our Princess Is In Another Castle!'"
-phrase = "'It's-a me, Mario!'"
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+phrase = 'It\'s-a me, Mario!'
 def mario
-  puts phrase
+  p phrase
 end
 
 def toadstool
